db10796be094cb7c85696b6bd9ec07c3eeff7437
jdchase112 <jdchase@hawaii.edu>
2026-05-05 10:16:32 -1000

    fixed header nav resize bug

--- art128/silvervillagerecords-final/js/script.js
+++ art128/silvervillagerecords-final/js/script.js
@@ -1,18 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     // OPEN AND CLOSE THE SITE NAV WHEN YOU CLICK ON THE MENU BUTTON
 
     const siteMenu = document.querySelector(".site-navigation");
     const menuButton = document.querySelector(".menu-button");
 
-    menuButton.onclick = ( ) => {
+    menuButton.onclick = () => {
 
-    // IF NAV IS CLOSED, THEN OPEN IT, ELSE CLOSE IT
-        if (siteMenu.getAttribute("data-navstate") === "closed") {
-        siteMenu.setAttribute("data-navstate","open");
+        // IF NAV IS CLOSED, THEN OPEN IT, ELSE CLOSE IT
+        if (siteMenu.getAttribute("data-navstate") === "open") {
+            siteMenu.setAttribute("data-navstate", "closed");
         } else {
-        siteMenu.setAttribute("data-navstate","closed");
+            siteMenu.setAttribute("data-navstate", "open");
         }
     };
-    
+
+    // REMOVE THE DATA-NAVSTATE ATTRIBUTE ON WINDOW RESIZE
+    window.addEventListener('resize', (event) => {
+        siteMenu.removeAttribute('data-navstate');
+    });
+
 });
